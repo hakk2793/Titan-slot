b8304716d2014d261b1c7eaf5b2b21dafc293eab
hakk2793 <69092488+hakk2793@users.noreply.github.com>
2026-02-25 13:12:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
 
+const CONFIG = {
+    RTP: 0.94,
+    BET: 1000,
+    REELS: 5,
+    ROWS: 3,
+    SYMBOLS: [
+        {icon:"💎", weight:5, pay:20},
+        {icon:"👑", weight:3, pay:50},
+        {icon:"⚡", weight:10, pay:10},
+        {icon:"🔥", weight:15, pay:5},
+        {icon:"🤖", weight:20, pay:3},
+        {icon:"🎰", weight:2, scatter:true}
+    ]
+};
